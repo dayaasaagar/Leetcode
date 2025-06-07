a19cba0090dabfd20fb86eac2370eec7377b199f
Dayaasaagar KMR <67572290+dayaasaagar@users.noreply.github.com>
2025-06-07 03:49:21 -0400

    Time: 8 ms (60.01%), Space: 18.9 MB (61.35%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,18 +1,16 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        countdic = Counter(s)
+        countdic= Counter(s)
 
-        h = [(-count,character) for character,count in countdic.items()]
+        h =[(-count,character) for character,count in countdic.items()]
         heapify(h)
         result=[]
+
         while h:
             count,character = heappop(h)
             result.append(character*-count)
-
+        
         return ''.join(result)
-
-
-
 
 
         
