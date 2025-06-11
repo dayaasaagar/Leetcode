7dbe4e83c2c75173a96d45b24dab23c2acc31b44
Dayaasaagar KMR <67572290+dayaasaagar@users.noreply.github.com>
2025-06-11 15:10:00 -0400

    Time: 1277 ms (77.59%), Space: 17.9 MB (22%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -3,13 +3,10 @@ class Solution:
         total=0
 
         for i in range(len(s)):
-            freq =[0]*26
+            freq = defaultdict(int)
             for j in range(i,len(s)):
-                idx = ord(s[j]) - ord('a')
-                freq[idx]+=1
-                max_f = max(freq)
-                min_f = min(f for f in freq if f>0)
-                total+= max_f-min_f
+                freq[s[j]]+=1
+                total+=max(freq.values())-min(freq.values())
         return total
 
 
