a0ede188b2e0b626223e804fb6cf24f0847d3485
Dayaasaagar KMR <67572290+dayaasaagar@users.noreply.github.com>
2025-07-01 00:57:28 -0400

    Time: 0 ms (100%), Space: 17.9 MB (28.11%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,21 +1,23 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        str_len=len(s)
-        char_count= Counter(s)
+        str_len= len(s)
+        char_count = Counter(s)
         max_freq = max(char_count.values())
 
         if max_freq > (str_len+1)//2:
             return ''
+        index =0
+        newstring = [None]*str_len
+
+        for char,count in char_count.most_common():
+            while count:
+                newstring[index]=char
+                count-=1
+                index+=2
+                if index>=str_len:
+                    index=1
+                
+
         
-        index = 0
-        rearrange=[None]*str_len
+        return ''.join(newstring)
 
-        for char,freq in char_count.most_common():
-            while freq:
-                rearrange[index]=char
-                freq-=1
-                index = index +2 
-
-                if index >= str_len:
-                    index =1 
-        return ''.join(rearrange)
