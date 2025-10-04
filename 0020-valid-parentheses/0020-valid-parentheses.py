6e1a5db61a69823552bf733218211f0fccb2a4c6
Dayaasaagar KMR <67572290+dayaasaagar@users.noreply.github.com>
2025-10-04 18:04:11 -0400

    Time: 0 ms (100%), Space: 17.8 MB (55.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,27 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        st=[]
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+
+
+        st = deque()
+
         for i in s:
-            
-            if i in ['(','{','[']:
+
+            if i == '(' or i=='[' or i=='{':
                 st.append(i)
-            elif(i==')' and not len(st)==0 and st[-1]=='('):
+            elif (i == ')' and not len(st)==0 and st[-1]=='('):
                 st.pop()
-            elif(i=='}' and not len(st)==0 and st[-1]=='{'):
+            elif (i == ']' and not len(st)==0 and st[-1]=='['):
                 st.pop()
-            elif(i==']' and not len(st)==0 and st[-1]=='['):
+            elif (i == '}' and not len(st)==0 and st[-1]=='{'):
                 st.pop()
             else:
                 return False
-        return st==[]  
+        
+        return True if len(st)==0 else False
+
+
+
+
+
+            
