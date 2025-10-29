348441aaaa73a274cb9293278be59198f543f026
Dayaasaagar KMR <67572290+dayaasaagar@users.noreply.github.com>
2025-10-29 14:23:36 -0400

    Time: 151 ms (90.16%), Space: 39.2 MB (83.63%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,12 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+        new_array = sorted(zip(position,speed),reverse= True)
+        stack = []
+        for pos,spd in new_array:
+            time =(target-pos) / spd 
+            if not stack or time > stack[-1]:
+                stack.append(time)
+        return len(stack)
+
+        
