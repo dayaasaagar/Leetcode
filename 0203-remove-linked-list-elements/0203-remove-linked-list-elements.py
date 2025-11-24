0ad9c99e6da5126a63c50f7bf9f3707334da04d4
Dayaasaagar KMR <67572290+dayaasaagar@users.noreply.github.com>
2025-11-23 20:07:41 -0500

    Time: 1 ms (48.9%), Space: 19.6 MB (81.23%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -6,13 +6,18 @@
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
 
-        if head is None:
-            return None
-        node = head
-        while node!=None:
-            if node.val!=val:
-                node = node.next
-        return head.next
+        dummy = ListNode(next=head)
+
+        prev=dummy
+        curr=head
+
+        while curr:
+            if curr.val==val:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = curr.next 
+        return dummy.next
 
             
         
