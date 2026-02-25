4b801b58cba99759e89502f1c53f53e1a547e7e3
Dayaasaagar KMR <67572290+dayaasaagar@users.noreply.github.com>
2026-02-25 16:55:42 -0500

    Time: 1 ms (60.53%), Space: 47.8 MB (57.88%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int trap(int[] height) {
+
+        if (height.length == 0) return 0;
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int lmax = 0;
+        int rmax = 0;
+
+        int res = 0;
+
+        while (left < right) {
+
+            if (height[left] <= height[right]) {
+                if (height[left] >= lmax) {
+                    lmax = height[left];
+                } else {
+                    res += lmax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rmax) {
+                    rmax = height[right];
+                } else {
+                    res += rmax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return res;
+    }
+}
