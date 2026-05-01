079eb52190b827d71df78a1e121982deecfc3d31
Dayaasaagar KMR <67572290+dayaasaagar@users.noreply.github.com>
2026-05-01 11:41:21 -0400

    Time: 1 ms (59.09%), Space: 48.8 MB (9.56%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -11,42 +11,68 @@ class Solution {
         //using two pointer technique
 
 
-        int leftMax=0;
-        int rightMax=0;
-        int left=0;
-        int right = height.length-1;
-        int res=0;
+        // int leftMax=0;
+        // int rightMax=0;
+        // int left=0;
+        // int right = height.length-1;
+        // int res=0;
 
 
-        while(left<right)
+        // while(left<right)
+        // {
+        //     if(height[left]<height[right])
+        //     {
+        //         if(height[left]>=leftMax)
+        //         {
+        //             leftMax=height[left];
+        //         }
+        //         else
+        //         {
+        //             res+=leftMax-height[left];
+        //         }
+        //         left++;
+        //     }
+        //     else
+        //     {
+        //         if(height[right]>=rightMax)
+        //         {
+        //             rightMax=height[right];
+        //         }
+        //         else
+        //         {
+        //             res+=rightMax-height[right];
+        //         }
+        //         right--;
+        //     }
+
+        // }
+
+        // return res;
+
+
+
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+        int ans=0;
+    
+
+        leftMax[0]=height[0]; //maximum of left of the oth element will be 0th element
+        for(int i=1;i<height.length;i++)
         {
-            if(height[left]<height[right])
-            {
-                if(height[left]>=leftMax)
-                {
-                    leftMax=height[left];
-                }
-                else
-                {
-                    res+=leftMax-height[left];
-                }
-                left++;
-            }
-            else
-            {
-                if(height[right]>=rightMax)
-                {
-                    rightMax=height[right];
-                }
-                else
-                {
-                    res+=rightMax-height[right];
-                }
-                right--;
-            }
+            leftMax[i]=Math.max(leftMax[i-1],height[i]);
+        }
+        rightMax[height.length-1]=height[height.length-1];
+        for(int k=height.length-2;k>=0;k--)
+        {
+            rightMax[k]=Math.max(rightMax[k+1],height[k]);
+        }
+        for(int j=0;j<height.length;j++)
+        {
+           ans+= Math.min(leftMax[j],rightMax[j])-height[j];
 
         }
 
-        return res;
+        return ans;
+
     }
 }
